aa0e12f204290ad32bd58bf7ac3d5963b4a6065f
David Garcia <joegar000@gmail.com>
2024-11-21 14:29:12 -0600

    Update virtual text config

--- nvim/.config/nvim/lua/plugins/dap/nvim-dap-virtual-text.lua
+++ nvim/.config/nvim/lua/plugins/dap/nvim-dap-virtual-text.lua
@@ -4,15 +4,7 @@ return {
   'theHamsta/nvim-dap-virtual-text',
   lazy = true,
   opts = {
-    -- Display debug text as a comment
-    -- commented = true,
-    -- Customize virtual text
-    display_callback = function(variable, buf, stackframe, node, options)
-      if options.virt_text_pos == 'inline' then
-        return ' = ' .. variable.value
-      else
-        return variable.name .. ' = ' .. variable.value
-      end
-    end,
+    virt_text_pos = 'eol',
+    clear_on_continue = true
   }
 }
