20d1075f2142eeccca5cf00f2ed6a8bc6175184c
David Garcia <joegar000@gmail.com>
2026-08-28 14:24:45 -0500

    Add guess-indent

---
+++ dot_config/nvim/lua/plugins/guess-indent.lua
@@ -0,0 +1,11 @@
+return {
+    "nmac427/guess-indent.nvim",
+    keys = { { "<leader>>", "<cmd>GuessIndent<CR>", desc = "Guess File Indentation" } },
+    event = "BufReadPost",
+    config = function()
+        require('guess-indent').setup({
+            filetype_exclude = { "dashboard", "NvimTree", "lazy", "mason", "netrw" }
+        })
+    end,
+    cond = not InVSCode
+}
