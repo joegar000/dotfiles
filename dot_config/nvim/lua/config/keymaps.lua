fd46b3c1ea04f828b7ed25f9e6d1746f48939bae
David Garcia <joegar000@gmail.com>
2026-08-27 20:42:56 -0500

    Re add custom keymaps

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Move selected lines up and down.
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move Selection Down" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move Selection Up" })
+
+-- Join the next line without moving the cursor.
+vim.keymap.set("n", "<S-Down>", "mzJ`z", { desc = "Join Line Below" })
+
+-- Disable Ex mode.
+vim.keymap.set("n", "Q", "<nop>")
+
+-- Return to Normal mode from a terminal.
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { desc = "Enter Normal Mode" })
